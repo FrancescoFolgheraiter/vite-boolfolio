655cba7ce125225342e407884df3c65a1529c217
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-13 18:05:36 +0100

    Bonus done, add nav for pagination, need to create a new component

---
+++ src/components/Paginate.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default{
+	data() {
+		return{
+			
+		};
+	},
+	components:{
+	
+	},
+    props:{
+ 
+    },
+	methods:{
+
+    },
+    mounted(){
+
+    }
+}
+
+</script>
+
+<template>
+ 
+</template>
+
+<style lang ="scss" scoped>
+
+</style>
